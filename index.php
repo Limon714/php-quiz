824528a07fb81c691f3174f8a0be1f593147f304
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-11-21 23:30:48 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="quiz.css">
+    <title>Super Global Variable</title>
+</head>
+<body>
+<div class="box">
+    <form action="function.php" method="post">
+
+<h3>What is the value of 4 + 5 = ?</h3>
+    <input type="number" name="ans1" id="">
+    <h3>What is the value of 20 - 5 = ?</h3>
+    <input type="number" name="ans2" id="">
+    <h3>What is the value of 4 x 5 = ?</h3>
+    <input type="number" name="ans3" id="">
+    <h3>What is the value of 40 &#247; 5 = ?</h3>
+    <input type="number" name="ans4" id="">
+    <br>
+    <br>
+         <input type="submit" value="Submit" id="btn">
+         </form>
+</div>
+    
+    
+</body>
+</html>
